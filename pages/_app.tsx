f8ffd7528ef58cc055b7846f16b7db8bafccee13
Shun Kushigami <shunku@amazon.com>
2023-06-01 01:09:30 +0900

    Added Cronitor for monitoring traffic.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useCronitor } from "@cronitorio/cronitor-rum-nextjs";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useCronitor("c12a9238601d732ad0291a4ebbfb0770");
   return (
     <Layout>
       <Component {...pageProps} />
